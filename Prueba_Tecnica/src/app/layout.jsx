9f0529ee49d036f4b30df4f210fce68ac65dc764
Pilar Baroni <122078706+PilarBaroni@users.noreply.github.com>
2024-08-20 21:53:28 -0300

    listo

--- Prueba_Tecnica/src/app/layout.jsx
+++ Prueba_Tecnica/src/app/layout.jsx
@@ -4,11 +4,36 @@ import "./global.css";
 
 export const metadata = {
   title: "Monkey D Luffy",
+  description: "Portafolio de Monkey D Luffy",
+  openGraph: {
+    title: "Monkey D Luffy",
+    description: "Portafolio de Monkey D Luffy.",
+    images: [
+      {
+        url: "https://portafolio-beige-one.vercel.app/YOPort.png",
+        width: 1200,
+        height: 630,
+        alt: "Portafolio de Monkey D Luffy",
+      },
+    ],
+    url: "https://portafolio-beige-one.vercel.app/",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Monkey D Luffy" />
+        <meta property="og:description" content="Portafolio de Monkey D Luffy" />
+        <meta property="og:image" content="..\public\LuffyPag.png" />
+        <meta property="og:url" content="https://monkey-d-luffy.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <title>Monkey D Luffy</title>
+      </head>
       <body className={styles.body}>
         <Navbar />
         {children}
