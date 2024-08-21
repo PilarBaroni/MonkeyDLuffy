80fbfb70adf48e6b054fa0ae017cf7319f299c64
Pilar Baroni <122078706+PilarBaroni@users.noreply.github.com>
2024-08-20 22:06:56 -0300

    imagen url

--- Prueba_Tecnica/src/app/layout.jsx
+++ Prueba_Tecnica/src/app/layout.jsx
@@ -1,7 +1,6 @@
 import { Navbar } from "./Components/Navbar/Navbar";
 import styles from "./index.module.css";
 import "./global.css";
-import LuffyPag from "../../public/LuffyPag.png";
 export const metadata = {
   title: "Monkey D Luffy",
   description: "Portafolio de Monkey D Luffy",
@@ -10,9 +9,7 @@ export const metadata = {
     description: "Portafolio de Monkey D Luffy.",
     images: [
       {
-        url: {
-          LuffyPag,
-        },
+        url:"https://monkey-d-luffy.vercel.app/",
         width: 1200,
         height: 630,
         alt: "Portafolio de Monkey D Luffy",
@@ -34,7 +31,7 @@ export default function RootLayout({ children }) {
           property="og:description"
           content="Portafolio de Monkey D Luffy"
         />
-        <meta property="og:image" content={LuffyPag} />
+        <meta property="og:image" content="../../public/LuffyPag.png" />
         <meta property="og:url" content="https://monkey-d-luffy.vercel.app/" />
         <meta property="og:type" content="website" />
         <title>Monkey D Luffy</title>
